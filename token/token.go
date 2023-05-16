22f1f2e17f03ba96a7bb9d7c5e6adf01f393b22d
Rohit <rohitp934@gmail.com>
2023-05-16 07:10:38 +0530

    Added keyword and ident support

--- token/token.go
+++ token/token.go
@@ -4,32 +4,45 @@ package token
 type TokenType string
 
 type Token struct {
-  Type TokenType
-  Literal string
+	Type    TokenType
+	Literal string
 }
 
 const (
-  ILLEGAL = "ILLEGAL"
-  EOF = "EOF"
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
 
-  // Identifiers + literals
-  IDENTIFIER = "IDENTIFIER" // add, foobar, x, y, ...
-  INT = "INT" // 1343456
+	// Identifiers + literals
+	IDENTIFIER = "IDENTIFIER" // add, foobar, x, y, ...
+	INT        = "INT"        // 1343456
 
-  // Operators
-  ASSIGN = "="
-  PLUS = "+"
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
 
-  // Delimiters
-  COMMA = ","
-  SEMICOLON = ";"
-  LPAREN = "("
-  RPAREN = ")"
-  LBRACE = "{"
-  RBRACE = "}"
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
 
-  // Keywords
-  LET = "LET"
-  FUNCTION = "FUNCTION"
-  RETURN = "RETURN"
+	// Keywords
+	LET      = "LET"
+	FUNCTION = "FUNCTION"
+	RETURN   = "RETURN"
 )
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"return": RETURN,
+}
+
+func LookupIdentifier(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENTIFIER
+}
