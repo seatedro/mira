1907c6643cfb2edc7afc6c2b3f7a6426627826e8
Rohit Prakash <rohitp934@gmail.com>
2023-06-13 12:30:50 +0530

    Added new operators

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -17,6 +17,8 @@ let add = fn(x, y) {
 };
 
 let result = add(five, ten);
+!-/*5;
+5 < 10 > 5;
 `
 
 	tests := []struct {
@@ -59,6 +61,18 @@ let result = add(five, ten);
 		{token.COMMA, ","},
 		{token.IDENTIFIER, "ten"},
 		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
--- token/token.go
+++ token/token.go
@@ -17,8 +17,14 @@ const (
 	INT        = "INT"        // 1343456
 
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	SLASH    = "/"
+	ASTERISK = "*"
+	BANG     = "!"
+	LT       = "<"
+	GT       = ">"
 
 	// Delimiters
 	COMMA     = ","
