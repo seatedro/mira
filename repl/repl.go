5fb91eccdf56da243366fb5e29f0144f2a2476f2
Rohit Prakash <rohitp934@gmail.com>
2023-07-30 12:51:54 +0530

    Fmt

--- repl/repl.go
+++ repl/repl.go
@@ -13,20 +13,20 @@ import (
 const PROMPT = "> "
 
 func Start(in io.Reader, out io.Writer) {
-  scanner := bufio.NewScanner(in)
+	scanner := bufio.NewScanner(in)
 
-  for {
-    fmt.Fprintf(out, PROMPT)
-    scanned := scanner.Scan()
-    if !scanned {
-      return
-    }
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
 
-    line := scanner.Text()
-    lex := lexer.New(line)
+		line := scanner.Text()
+		lex := lexer.New(line)
 
-    for tok := lex.NextToken(); tok.Type != token.EOF; tok = lex.NextToken() {
-      fmt.Fprintf(out, "%+v\n", tok)
-    }
-  }
+		for tok := lex.NextToken(); tok.Type != token.EOF; tok = lex.NextToken() {
+			fmt.Fprintf(out, "%+v\n", tok)
+		}
+	}
 }
